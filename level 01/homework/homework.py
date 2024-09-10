4a89ad2810fa8274587853cc03c2c7032af9333f
Irakli-12 <gegiairakli13@gmail.com>
2024-09-10 15:55:07 +0400

    commit

---
+++ level 01/homework/homework.py
@@ -0,0 +1,5 @@
+name = "irakli"
+surname = "gegia"
+age = 11
+country = "georgia,"
+print (name + " " + surname + " " + "is" + " " + str(age) + " " + "years old ." + " " +  "he lives in martvili"  + " " + country)
