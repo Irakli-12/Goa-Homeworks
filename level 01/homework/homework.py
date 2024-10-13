1989afcaf6b339ac6ee75e6c6b982a99f3065690
Irakli-12 <gegiairakli13@gmail.com>
2024-10-13 22:20:50 +0400

    commit

--- level 01/homework/homework.py
+++ level 01/homework/homework.py
@@ -3,3 +3,6 @@ surname = "gegia"
 age = 11
 country = "georgia,"
 print (name + " " + surname + " " + "is" + " " + str(age) + " " + "years old ." + " " +  "he lives in martvili"  + " " + country)
+
+list1=["ikako", 12, ]
+print(list1[1])
